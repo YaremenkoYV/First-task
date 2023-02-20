2ab65aabd070351e19fac49dfc28a2a4b52bd099
YaremenkoYV <125539657+YaremenkoYV@users.noreply.github.com>
2023-02-20 20:50:30 +0200

    Add files via upload

---
+++ 1.4.py
@@ -0,0 +1,11 @@
+a = int(input('Введіть число яке хочете поділити: '))
+b = int(input('Введіть число на яке хочете поділити: '))
+sum_=a
+print(sum_)
+for i in range(a):
+    sum_=sum_-b
+
+    print(sum_)
+    if sum_<=b:
+        print('Ділення завершено')
+        break
