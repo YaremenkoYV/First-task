e5063dbf19b4a3b793ef5f95fd21c3561a2f65d4
Yaremenko Yurii <125539657+YaremenkoYV@users.noreply.github.com>
2023-05-01 17:02:10 +0300

    Update 1.3.py

--- 1.3.py
+++ 1.3.py
@@ -1,6 +1,8 @@
 a = int(input('Введіть число яке хочете помножити: '))
 b = int(input('Введіть число на яке хочете помножити: '))
-sum_=0
-for i in range(b):
-    sum_+=a
-    print(sum_)
+sum_ = 0
+i = 0
+while i < b:
+    sum_ += a
+    i += 1
+print(sum_)
